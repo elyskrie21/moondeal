2f7cda99570e0317eb618a682a10bcb12afaed44
Elyse Kriegel <esamkriegel@gmail.com>
2022-04-03 20:44:22 -0500

    change CI value

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import * as serviceWorker from "./serviceWorker";
-import App from './app'; 
+import App from './App'; 
 
 
 ReactDOM.render(
