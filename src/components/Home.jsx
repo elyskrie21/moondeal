b6e2119798359e7bc62c0f87e6ddd8b63c98d604
Casper <casperr678@gmail.com>
2022-04-23 20:00:09 -0500

    Added test image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
                 allows you to add your favorite products to our dashboard and we will
                 send an email notification out whenever they go on sale.
               </p>
-              <img src="testimage.jpg" width="400px" height="400px"/>
+              <img className="image" src="testimage.jpg" width="400px" height="400px"/>
             </div>
             <div className="d-text">
               <h1 className="d-text"></h1>
