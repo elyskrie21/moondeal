50f524b5a41a7bcf4f0a71d5df8e503fb4abff7a
Casper <casperr678@gmail.com>
2022-04-30 16:12:40 -0500

    Added gifs, testimonys, and other visuals

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,31 +10,32 @@ function Home() {
           <div className="row">
             <div className="col-6">
               <img className="gif" src="amazon gif.gif" />
-              <p className="title">Works with your favorite online stores</p>
-              {/* <h1 className="pt-5">Shop with Confidence</h1>
-              <p>
-                "MoonDeal allows me to get the best deals on my purchases
-                without having to constantly check my wishlist" - Jessica
-              </p>
-              <p>
-                "Thank you MoonDeal for making it easier to find my favorite
-                products on sale" - Kayla
-              </p> */}
-              <a href="https://www.google.com/" target="_blank">
-                <button className="download-button">Download Moondeal</button>
-              </a>
+              <p className="title">Compatible with your favorite online stores</p>
+              <img className="a-logo" src="amazonlogo.png"/>
+              <img className="e-logo" src="ebaylogo.png"/>
+              <img className="gif mt-5" src="search gif.gif"/>
             </div>
+
             <div className="col-6">
               <h1 className="home-title">Welcome to MoonDeal</h1>
               <p className="home-text">
                 "Serving you deals to the moon and back"
               </p>
-              <img className="gif" src="ebay gif.gif"/>
+              <img className="gif" src="ebay gif.gif" />
+              <h1 className="pt-5">Shop with Confidence</h1>
+              <p>
+                "MoonDeal allows me to get the best deals on my purchases
+                without having to constantly check my wishlist" - Jessica<img className= "p-photo" src="Stock Female Photo.png"/>
+              </p>
+              <p>
+                "Thank you MoonDeal for making it easier to find my favorite
+                products on sale" - Jake
+              </p>
             </div>
-            <a href="https://www.google.com/" target="_blank">
-                <button className="download-button">Download Moondeal</button>
-            </a>
           </div>
+          {/* <a href="https://www.google.com/" target="_blank">
+                <button className="download-button">Download Moondeal</button>
+          </a> */}
         </div>
       </div>
       <Footer />
