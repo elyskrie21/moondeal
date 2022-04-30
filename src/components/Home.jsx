f966c2bd2363c0d4afbd886462b0770904961fac
Casper <casperr678@gmail.com>
2022-04-30 02:30:52 -0500

    Added Gifs, and did some work on the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,32 +6,36 @@ function Home() {
     <div>
       <Navigation />
       <div className="moon-gradient">
-        <div className="container pt-5">
-        <div className="row">
-          <div className="col-6">
-            <h1 className="">Welcome to MoonDeal</h1>
-            <p className="">
-              MoonDeal is a intuitive solution to online shopping. Our extension
-              allows you to add your favorite products to our dashboard and we
-              will send an email notification out whenever they go on sale.
-            </p>
+        <div className="container pt-5 text-center text-light">
+          <div className="row">
+            <div className="col-6">
+              <img className="gif" src="amazon gif.gif" />
+              <p className="title">Works with your favorite online stores</p>
+              {/* <h1 className="pt-5">Shop with Confidence</h1>
+              <p>
+                "MoonDeal allows me to get the best deals on my purchases
+                without having to constantly check my wishlist" - Jessica
+              </p>
+              <p>
+                "Thank you MoonDeal for making it easier to find my favorite
+                products on sale" - Kayla
+              </p> */}
+              <a href="https://www.google.com/" target="_blank">
+                <button className="download-button">Download Moondeal</button>
+              </a>
+            </div>
+            <div className="col-6">
+              <h1 className="home-title">Welcome to MoonDeal</h1>
+              <p className="home-text">
+                "Serving you deals to the moon and back"
+              </p>
+              <img className="gif" src="ebay gif.gif"/>
+            </div>
             <a href="https://www.google.com/" target="_blank">
-              <button className="download-button">Download MoonDeal</button>
+                <button className="download-button">Download Moondeal</button>
             </a>
-            <p className="">
-              "MoonDeal allows me to get the best deals on my purchases without
-              having to constantly check my wishlist" - Jessica
-            </p>
-            <p>
-              "Thank you MoonDeal for making it easier to find my favorite
-              products on sale" - Kayla
-            </p>
-          </div>
-          <div className="col-6">
-            <img className="image" src="Extension-image.png" />
           </div>
         </div>
-      </div>
       </div>
       <Footer />
     </div>
